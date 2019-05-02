70d621f5b64f5f1e65b3e3cf70f95eaa8cc6403f
fakush <fcreus@gmail.com>
2019-05-02 12:11:50 -0300

    Lista Version 2.5 Cambios Gráficos - Proximos Pasos Borrar Archivos no utilizados. Lector Código de Barras. Funciona QR, no funciona Barcode

--- app/src/main/java/ar/com/fcapps/controlprecioscuidados/LectorCodigoDeBarras.java
+++ app/src/main/java/ar/com/fcapps/controlprecioscuidados/LectorCodigoDeBarras.java
@@ -3,13 +3,68 @@ package ar.com.fcapps.controlprecioscuidados;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.widget.TextView;
+
+import github.nisrulz.qreader.QRDataListener;
+import github.nisrulz.qreader.QREader;
 
 public class LectorCodigoDeBarras extends AppCompatActivity {
+
+    // QREader
+    private SurfaceView mySurfaceView;
+    private QREader qrEader;
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lector_codigo_de_barras);
+
+        text = findViewById(R.id.textoCodigo);
+
+        // Setup SurfaceView
+        // -----------------
+        mySurfaceView = (SurfaceView) findViewById(R.id.camera_view);
+
+        // Init QREader
+        // ------------
+        qrEader = new QREader.Builder(this, mySurfaceView, new QRDataListener() {
+            @Override
+            public void onDetected(final String data) {
+                Log.d("QREader", "Value : " + data);
+                text.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        text.setText(data);
+                    }
+                });
+            }
+        }).facing(QREader.BACK_CAM)
+                .enableAutofocus(true)
+                .height(mySurfaceView.getHeight())
+                .width(mySurfaceView.getWidth())
+                .build();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Init and Start with SurfaceView
+        // -------------------------------
+        qrEader.initAndStart(mySurfaceView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Cleanup in onPause()
+        // --------------------
+        qrEader.releaseAndCleanup();
     }
 
 }
